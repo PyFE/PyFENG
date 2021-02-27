44a4418aed9ffe59c040aab565f458b3a8394b1f
Cedric Qi <cescdf@icloud.com>
2021-02-27 22:41:21 +0800

    some changes

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name='pyfeng',
+    name='pyfeng-YOUR-USERNAME',
     version='0.1.0',
     description='Python Financial Engineering',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Jaehyuk Choi',
     author_email='pyfe@eml.cc',
     url='https://github.com/PyFE/PyFENG',
