ec9b9e5470ff1acd2ff9e52b34ab68e5b009c7ad
LantianXue <xuelantian@foxmail.com>
2021-04-28 09:50:07 +0800

    update the sample class
    remove the .ipynb file, create our sample class

---
+++ pyfeng/Ju2002_Basekt_Asian.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 28 09:08:29 2021
+
+@author: Lantian
+"""
+
+class Ju2002_Basket_Asian:
+    def _init_(self, price, sigma, rate, texp, rho, did, weight, exep):
+        self.price = price
+        self.sigma = sigma
+        self.rate = rate
+        self.texp = texp
+        self.rho = rho
+        self.did = did
+        self.weight = weight
+        self.exep = exep
+        
+    def u1():
+        #the first momentum of log normal distribution#
+        pass
+    
+    def u2():
+        #the second momentum of log normal distribution#
+        pass
+    
+    def u2_1st_der():
+        ##
+        pass
+    
+    def u2_2nd_der():
+        
+        pass
+    
+    def u2_3rd_der():
+        pass
+    
+    def ak_bar():
+        #calculate the average a, save to self#
+        pass
+    
+    def e_a12_a2():
+        pass
+    
+    def e_a12_a22():
+        pass
+    
+    def e_a13_a3():
+        pass
+    
+    def e_a1_a2_a3():
+        pass
+    
+    def func_a():
+        #the function #
+        pass
+    
+    def func_b():
+        pass
+    
+    def func_c():
+        pass
+    
+    def func_d():
+        pass
