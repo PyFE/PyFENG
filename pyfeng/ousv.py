824fd84e4726d0ec02e713b3b704f30c760b60f6
Panyu Li <2001212358@stu.pku.edu.cn>
2021-04-29 11:10:42 +0800

    update on 0429

--- pyfeng/ousv.py
+++ pyfeng/ousv.py
@@ -24,47 +24,48 @@ class OusvIft(sv.SvABC):
         # implement the formula for D(t,T), B(t,T), C(t,T) in paper appendix
         mr, theta, vov = self.mr, self.theta, self.vov
 
-        gamma1 = np.sqrt(2*vov**2*s1 + mr**2)
-        gamma2 = (mr - 2*vov**2*s3)/gamma1
-        gamma3 = mr**2*theta - s2*vov**2
-        sinh = np.sinh(gamma1*texp)
-        cosh = np.cosh(gamma1*texp)
-        sincos = sinh + gamma2*cosh
-        cossin = cosh + gamma2*sinh
-        ktg3 = mr*theta*gamma1 - gamma2*gamma3
-        s2g3 = vov**2*gamma1**3
+        gamma1 = np.sqrt(2 * vov ** 2 * s1 + mr ** 2)
+        gamma2 = (mr - 2 * vov ** 2 * s3) / gamma1
+        gamma3 = mr ** 2 * theta - s2 * vov ** 2
+        sinh = np.sinh(gamma1 * texp)
+        cosh = np.cosh(gamma1 * texp)
+        sincos = sinh + gamma2 * cosh
+        cossin = cosh + gamma2 * sinh
+        ktg3 = mr * theta * gamma1 - gamma2 * gamma3
+        s2g3 = vov ** 2 * gamma1 ** 3
 
-        D = (mr - gamma1*sincos/cossin) / vov**2
-        B = ((ktg3+gamma3*sincos)/cossin - mr*theta*gamma1) / (vov**2*gamma1)
-        C = -0.5*np.log(cossin) + 0.5*mr*texp + ((mr*theta*gamma1)**2-gamma3**2)/(2*s2g3)*(sinh/cossin-gamma1*texp) \
-            + ktg3*gamma3/s2g3*((cosh-1)/cossin)
+        D = (mr - gamma1 * sincos / cossin) / vov ** 2
+        B = ((ktg3 + gamma3 * sincos) / cossin - mr * theta * gamma1) / (vov ** 2 * gamma1)
+        C = -0.5 * np.log(cossin) + 0.5 * mr * texp + ((mr * theta * gamma1) ** 2 - gamma3 ** 2) / (2 * s2g3) * (
+                    sinh / cossin - gamma1 * texp) \
+            + ktg3 * gamma3 / s2g3 * ((cosh - 1) / cossin)
 
         return D, B, C
 
     def f_1(self, phi, fwd, texp):
         # implement the formula (12)
         mr, theta, vov, rho = self.mr, self.theta, self.vov, self.rho
-        tmp = 1 + 1j*phi
-        s1 = 0.5*tmp*(-tmp*(1-rho**2) + (1-2*mr*rho/vov))
-        s2 = tmp*mr*theta*rho/vov
-        s3 = 0.5*tmp*rho/vov
+        tmp = 1 + 1j * phi
+        s1 = 0.5 * tmp * (-tmp * (1 - rho ** 2) + (1 - 2 * mr * rho / vov))
+        s2 = tmp * mr * theta * rho / vov
+        s3 = 0.5 * tmp * rho / vov
 
-        res = 1j*phi*np.log(fwd) - 0.5*rho*(1+1j*phi)*(self.sigma**2/vov+vov*texp)
+        res = 1j * phi * np.log(fwd) - 0.5 * rho * (1 + 1j * phi) * (self.sigma ** 2 / vov + vov * texp)
         D, B, C = self.D_B_C(s1, s2, s3, texp)
-        res += 0.5*D*self.sigma**2 + B*self.sigma + C
+        res += 0.5 * D * self.sigma ** 2 + B * self.sigma + C
         return np.exp(res)
 
     def f_2(self, phi, fwd, texp):
         # implement the formula (13)
         mr, theta, vov, rho = self.mr, self.theta, self.vov, self.rho
 
-        s1 = 0.5*(phi**2*(1 - rho**2) + 1j*phi*(1 - 2*mr*rho/vov))
-        s2 = 1j*phi*mr*theta*rho/vov
-        s3 = 0.5*1j*phi*rho/vov
+        s1 = 0.5 * (phi ** 2 * (1 - rho ** 2) + 1j * phi * (1 - 2 * mr * rho / vov))
+        s2 = 1j * phi * mr * theta * rho / vov
+        s3 = 0.5 * 1j * phi * rho / vov
 
-        res = 1j*phi*np.log(fwd) - 0.5*1j*phi*rho*(self.sigma**2/vov + vov*texp)
+        res = 1j * phi * np.log(fwd) - 0.5 * 1j * phi * rho * (self.sigma ** 2 / vov + vov * texp)
         D, B, C = self.D_B_C(s1, s2, s3, texp)
-        res += 0.5*D*self.sigma**2 + B*self.sigma + C
+        res += 0.5 * D * self.sigma ** 2 + B * self.sigma + C
         return np.exp(res)
 
     def price(self, strike, spot, texp, cp=1):
@@ -73,24 +74,112 @@ class OusvIft(sv.SvABC):
 
         log_k = np.log(strike)
         J, h = 100001, 0.001
-        phi = (np.arange(J)[:, None] + 1)*h  # shape=(J,1)
-        ff1 = 0.5 + 1/np.pi*scint.simps((self.f_1(phi, fwd, texp)*np.exp(-1j*phi*log_k)/(1j*phi)).real, dx=h, axis=0)
-        ff2 = 0.5 + 1/np.pi*scint.simps((self.f_2(phi, fwd, texp)*np.exp(-1j*phi*log_k)/(1j*phi)).real, dx=h, axis=0)
+        phi = (np.arange(J)[:, None] + 1) * h  # shape=(J,1)
+        ff1 = 0.5 + 1 / np.pi * scint.simps((self.f_1(phi, fwd, texp) * np.exp(-1j * phi * log_k) / (1j * phi)).real,
+                                            dx=h, axis=0)
+        ff2 = 0.5 + 1 / np.pi * scint.simps((self.f_2(phi, fwd, texp) * np.exp(-1j * phi * log_k) / (1j * phi)).real,
+                                            dx=h, axis=0)
 
-        price = np.where(cp > 0, fwd*ff1 - strike*ff2, strike*(1-ff2) - fwd*(1-ff1))
+        price = np.where(cp > 0, fwd * ff1 - strike * ff2, strike * (1 - ff2) - fwd * (1 - ff1))
         if len(price) == 1:
             price = price[0]
 
-        return df*price
+        return df * price
+
 
 class OusvCondMC(sv.SvABC, sv.CondMcBsmABC):
     """
         OUSV model with conditional Monte-Carlo simulation
         """
 
-    def vol_paths(self, tobs):
-        return 0
+    def _bm_incr(self, tobs, cum=False, n_path=None):
+        """
+        Calculate incremental Brownian Motions
 
-    def cond_fwd_vol(self, texp):
-        return 0
+        Args:
+            tobs: observation times (array). 0 is not included.
+            cum: return cumulative values if True
+            n_path: number of paths. If None (default), use the stored one.
+
+        Returns:
+            price path (time, path)
+        """
+        # dt = np.diff(np.atleast_1d(tobs), prepend=0)
+        n_dt = len(tobs)
+
+        tobs_lag = tobs[:-1]
+        tobs_lag = np.insert(tobs_lag, 0, 0)
+        bm_var = np.exp(tobs) - np.exp(tobs_lag)
+        n_path = n_path or self.n_path
+
+        if self.antithetic:
+            # generate random number in the order of path, time, asset and transposed
+            # in this way, the same paths are generated when increasing n_path
+            bm_incr = self.rng.normal(size=(int(n_path/2), n_dt)).T * np.sqrt(bm_var[:, None])
+            bm_incr = np.stack([bm_incr, -bm_incr], axis=-1).reshape((-1, n_path))
+        else:
+            bm_incr = np.random.randn(n_path, n_dt).T * np.sqrt(bm_var[:, None])
+
+        if cum:
+            np.cumsum(bm_incr, axis=0, out=bm_incr)
+
+        return bm_incr
+
+    def vol_paths(self, tobs, kai, LT_avg=0):
+        """
+        sigma_t = np.exp(-alpha * tobs) * (sigma0 - sigma_final + vov / np.sqrt(2 * alpha) * bm) + sigma_final
+        Args:
+            tobs: observation time (array)
+            kai: coefficient of dt
+            LT_avg: the long term average
+            mu: rn-derivative
+
+        Returns: volatility path (time, path) including the value at t=0
+        """
+        bm_path = self._bm_incr(tobs, cum=True)  # B_s (0 <= s <= 1)
+        sigma_t = LT_avg + (self.sigma - LT_avg + self.vov / np.sqrt(2 * kai) * bm_path) * np.exp(-kai * tobs[:, None])
+        sigma_t = np.insert(sigma_t, 0, np.array([self.sigma] * sigma_t.shape[1]), axis=0)
+        return sigma_t
+
+    def cond_fwd_vol(self, texp, kai, LT_avg):
+        """
+            Returns new forward and volatility conditional on volatility path (e.g., sigma_T, integrated variance)
+            The forward and volatility are standardized in the sense that F_0 = 1 and sigma_0 = 1
+            Therefore, they should be scaled by the original F_0 and sigma_0 values
+
+            Args:
+                LT_avg: the long term average
+                texp: time-to-expiry
+
+            Returns: (forward, volatility)
+        """
+        rhoc = np.sqrt(1.0 - self.rho ** 2)
+        tobs = self.tobs(texp)
+        n_dt = len(tobs)
+        sigma_paths = self.vol_paths(tobs, kai, LT_avg)
+        sigma_final = sigma_paths[-1, :]
+        int_sigma = scint.simps(sigma_paths, dx=1, axis=0) / n_dt
+        int_var = scint.simps(sigma_paths ** 2, dx=1, axis=0) / n_dt
+
+        fwd_cond = np.exp(self.rho * ((sigma_final**2 - self.sigma**2) / (2 * self.vov) - self.vov * 0.5 * texp -
+                                      kai * LT_avg / self.vov * int_sigma +
+                                      (kai/self.vov - self.rho * 0.5) * int_var))   # scaled by initial value
+
+        vol_cond = rhoc * np.sqrt(int_var / texp)
+
+        return fwd_cond, vol_cond
+
+    def price(self, strike, spot, texp, kai, LT_avg, cp=1):
+        kk = strike / spot
+        scalar_output = np.isscalar(kk)
+        kk = np.atleast_1d(kk)
+
+        fwd_cond, vol_cond = self.cond_fwd_vol(texp, kai, LT_avg)
+
+        base_model = self.base_model(vol_cond)
+        price_grid = base_model.price(kk[:, None], fwd_cond, texp=texp, cp=cp)
+
+        price = spot * np.mean(price_grid, axis=1)
+
+        return price[0] if scalar_output else price
 
