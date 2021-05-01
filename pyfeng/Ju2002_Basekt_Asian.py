84e964f866d4101c1fb83bd80ea85e67c3026bd2
LantianXue <xuelantian@foxmail.com>
2021-05-01 23:01:36 +0800

    complete implementation of some functions

--- pyfeng/Ju2002_Basekt_Asian.py
+++ pyfeng/Ju2002_Basekt_Asian.py
@@ -21,59 +21,128 @@ class Ju2002_Basket_Asian(NormBasket):
             is_fwd: if True, treat `spot` as forward price. False by default.
         """
     def average_s(self, spot, texp):
-        a_s = self.weight @ self.spot @ np.exp(self.divr*texp)
-        return a_s
+        #cal the forward price of asset num in the basket
+        av_s = np.zeros(len(self.weight))
+        for num in range(len(self.weight)):
+            av_s.append(self.weight[num]*spot[num]*np.exp((self.intr-self.divr[num])*texp))
+        self.av_s = av_s
     
-    def averge_rho(self):
-        pass
+    def averge_rho(self, texp):
+        #cal the rho between asset i and j
+        av_rho = np.zeros((len(self.weight),len(self.weight)))
+        for i in range(len(self.weight)):
+            for j in range(len(self.weight)):
+                av_rho[i,j] = self.cor[i,j]*self.sigma[i]*self.sigma[j]*texp
+        self.av_rho = av_rho
     
-    def u1(self):
+    def u1(self, spot, texp):
         #the first momentum of log normal distribution#
-        pass
+        u1_value = self.weight @ (spot * np.exp((self.intr-self.divr)*texp))
+        return u1_value
     
-    def u2():
+    def u2(self, z):
         #the second momentum of log normal distribution#
-        pass
+        u2_value = 0
+        for i in range(len(self.weight)):
+            for j in range(len(self.weight)):
+              u2_value += self.av_s[i] * self.av_s[j] *np.exp(z*z*self.av_rho[i,j])
+        return u2_value
     
-    def u2_1st_der():
-        ##
-        pass
+    def u2_1st_der(self):
+        u2_1st_value = 0
+        for i in range(len(self.weight)):
+            for j in range(len(self.weight)):
+              u2_1st_value += self.av_s[i] * self.av_s[j] *self.av_rho[i,j]
+        return u2_1st_value
     
-    def u2_2nd_der():
-        
-        pass
+    def u2_2nd_der(self):
+        u2_2nd_value = 0
+        for i in range(len(self.weight)):
+            for j in range(len(self.weight)):
+              u2_2nd_value += self.av_s[i] * self.av_s[j] *pow(self.av_rho[i,j],2)
+        return u2_2nd_value
     
-    def u2_3rd_der():
-        pass
+    def u2_3rd_der(self):
+        u2_3rd_value = 0
+        for i in range(len(self.weight)):
+            for j in range(len(self.weight)):
+              u2_3rd_value += self.av_s[i] * self.av_s[j] *pow(self.av_rho[i,j],3)
+        return u2_3rd_value
     
-    def ak_bar():
+    def ak_bar(self):
         #calculate the average a, save to self#
+        av_a = self.av_rho @ self.av_s
+        self.av_a = av_a
+    
+    def e_a12_a2(self):
+        return 2 * self.av_s @ pow(self.av_a,2)
+    
+    def e_a12_a22(self):
+        value = 0
+        for i in range(len(self.weight)):
+            for j in range(len(self.weight)):
+                value += self.av_a[i]*self.av_s[i]*self.av_rho[i,j]*self.av_a[j]*self.av_s[j]
+        value *=8
+        value += 2*self.u2_1st_der()*self.u2_2nd_der()
+        return value
+    
+    def e_a13_a3(self):
+        return 6* self.av_s @ pow(self.av_a,3)
+    
+    def e_a1_a2_a3(self):
+        value = 0
+        for i in range(len(self.weight)):
+            for j in range(len(self.weight)):
+                value += self.av_s[i]*pow(self.av_rho[i,j],2)*self.av_a[j]*self.av_s[j]
+        value *= 6
+        return value
+    
+    def e_a23(self):
+        value = 0
+        temp = np.zeros((len(self.weight),len(self.weight)))
+        for i in range(len(self.weight)):
+            for j in range(len(self.weight)):
+                temp[i,j] = pow(self.av_s[i],0.5)*self.av_rho[i,j]*pow(self.av_s[j],0.5)
+        for i in range(len(self.weight)):
+            for j in range(len(self.weight)):
+                for k in range(len(self.weight)):
+                    value += temp[i,j]*temp[j,k]*temp[k,i]
+        value *= 8
+        return value
+    
+    def func_a1(self, z):
+        return -pow(z,2)*self.u2_1st_der()/2/self.u2(0)
+    
+    def func_a2(self, z):
+        return 2*pow(self.func_a1(z),2)-pow(z,4)*self.u2_2nd_der()/2/self.u2(0)
+    
+    def func_a3(self, z):
+        return 6*self.func_a1(z)*self.func_a2(z)-4*pow(self.func_a1(z),3)-pow(z,6)*self.u2_3rd_der()/2/self.u2(0)
+    
+    def func_b1(self, z):
+        pass
+        # return pow(z,4)*self.e_a12_a2()/4/
+        
+    def func_b2(self, z):
+        return pow(self.func_a1(z),2)-self.func_a2(z)/2
+    
+    def func_c1(self, z):
+        return -self.func_a1(z)*self.func_b1(z)
+    
+    def func_c2(self, z):
         pass
     
-    def e_a12_a2():
-        pass
+    def func_c3(self, z):
+       pass
     
-    def e_a12_a22():
-        pass
-    
-    def e_a13_a3():
-        pass
-    
-    def e_a1_a2_a3():
-        pass
-    
-    def func_a():
-        #the function #
-        pass
-    
-    def func_b():
-        pass
-    
-    def func_c():
-        pass
+    def func_c4(self, z):
+        return self.func_a1(z)*self.func_a2(z)-2*pow(self.func_a1(z),3)/3-self.func_a3(z)/6
     
     def func_d():
         pass
     
     def price(self, strike, spot, texp, cp=1):
-        pass
+        self.average_s(spot, texp)
+        self.averge_rho(texp)
+        # to be continue
+        return 0
--- pyfeng/jutest.py
+++ pyfeng/jutest.py
@@ -4,9 +4,17 @@ Created on Sat May  1 20:17:09 2021
 
 @author: Lantian
 """
-import Ju2002_Basekt_Asian
 
-
-__package__ ='pyfeng'
-x = Ju2002_Basekt_Asian()
+class test():
+    def __init__ (self, a, b):
+        self.a = a
+        self.b = b
+        
+    def minus(self):
+        return self.a -self.b
     
+    def plus(self):
+        return self.minus()+2*self.b
+
+a = test(1,2)
+print(a.plus())
