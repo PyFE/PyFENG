686f5573b3796099ed794411fec3182d7b7109d4
Hejinzefinance <Hejinze_finance@pku.edu.cn>
2021-05-09 11:12:30 +0800

    Update __init__.py

--- pyfeng/__init__.py
+++ pyfeng/__init__.py
@@ -1,19 +1,14 @@
 from .norm import Norm  # the order is sensitive because of `price_barrier` method. Put it before .bsm
 from .bsm import Bsm, BsmDisp
 from .cev import Cev
-from .gamma import InvGam, InvGauss
+from .gamma import InvGam
 from .sabr import SabrHagan2002, SabrNorm, SabrLorig2017, SabrChoiWu2021H, SabrChoiWu2021P
 from .sabr_int import SabrUncorrChoiWu2021
 from .sabr_mc import SabrCondMc
 from .nsvh import Nsvh1, NsvhMc
 from .multiasset import BsmSpreadKirk, BsmSpreadBjerksund2014, NormBasket, NormSpread, BsmBasketLevy1992, BsmMax2, \
-    BsmBasketMilevsky1998, BsmBasket1Bm
+    BsmBasketMilevsky1998
 from .multiasset_mc import BsmNdMc, NormNdMc
 from .heston import HestonCondMc
 from .ousv import OusvIft
-from .Ju2002_Basket_Asian import BsmBasketAsianJu2002, BsmContinuousAsianJu2002
-from .ousv import OusvIft, OusvCondMC
-from .garch import GarchCondMC, GarchApproxUncor
-from .heston_cmc_qe import HestonCondMcQE
-from .sv32_cmc_qe import Sv32CondMcQE
 from .sv32McAe import Sv32McAe
