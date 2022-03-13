04868f5b968ec14ba46c05e6f350c944177efb76
jaehyukchoi <jaehyukchoi@users.noreply.github.com>
2022-03-13 21:03:39 +0800

    Version up

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pyfeng",
-    version="0.1.8",
+    version="0.1.9",
     description="Python Financial Engineering",
     long_description=long_description,
     long_description_content_type="text/markdown",
