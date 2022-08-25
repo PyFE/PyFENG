analytic variance swap corrected

pyfeng/heston.py
@@ -26,7 +26,7 @@ class HestonABC(sv.SvABC, abc.ABC):
         s2 *= self.vov**2*(1 - expo)/self.mr
         return m, s2
 
-    def avgvar_mv(self, var0, texp):
+    def avgvar_mv(self, texp, var0=None):
         """
         Mean and variance of the average variance given V(0) = var_0.
         Appnedix B in Ball & Roma (1994)
@@ -39,46 +39,53 @@ class HestonABC(sv.SvABC, abc.ABC):
             mean, variance
         """
 
+        ### We take var0 as argument instead of using self.sigma
+        ### because it is used in var_step_qe method in HestonMcAndersen2008
+        var0 = var0 or self.sigma
+
         mr_t = self.mr*texp
-        e_mr = np.exp(-mr_t)
+        e_mr_t = np.exp(-mr_t)
         x0 = var0 - self.theta
-        mean = self.theta + x0*(1 - e_mr)/mr_t
-        var = (self.theta - 2*x0*e_mr) + (1 - e_mr)*(var0 - 2.5*self.theta + (var0 - self.theta/2)*e_mr)/mr_t
+        mean = self.theta + x0*(1 - e_mr_t)/mr_t
+        var = (self.theta - 2*x0*e_mr_t) + (1 - e_mr_t)*(var0 - 2.5*self.theta + (var0 - self.theta/2)*e_mr_t)/mr_t
         var *= (self.vov/mr_t)**2 * texp
         return mean, var
 
-    def fair_strike_var(self, texp, aa):
+    def varswap(self, texp, n_per_yr):
         """
-        Analytic fair strike of variance swap
+        Analytic fair strike of variance swap. Eq (11) in Bernard & Cui (2014)
 
         Args:
             texp: time to expiry
-            aa: number of observation per year
+            n_per_yr: number of observation per year. If None, continuous monitoring
 
         Returns:
             Fair strike
+
+        References:
+            - Bernard C, Cui Z (2014) Prices and Asymptotics for Discrete Variance Swaps. Applied Mathematical Finance 21:140–173. https://doi.org/10.1080/1350486X.2013.820524
         """
 
         var0 = self.sigma
 
         ### continuously monitored fair strike (same as mean of avgvar)
         mr_t = self.mr*texp
-        e_mr = np.exp(-mr_t)
+        e_mr_t = np.exp(-mr_t)
         x0 = var0 - self.theta
-        strike = self.theta + x0*(1 - e_mr)/mr_t
+        strike = self.theta + x0*(1 - e_mr_t)/mr_t
 
-        if aa is not None:
+        if n_per_yr is not None:
             ### adjustment for discrete monitoring
-            mr_a = self.mr/aa
+            mr_a = self.mr/n_per_yr
             e_mr_a = np.exp(-mr_a)
 
             tmp = self.theta - 2*self.intr
-            strike += tmp / (4*aa) * (tmp + 2*x0*(1 - e_mr)/mr_t)
+            strike += tmp/(4*n_per_yr)*(tmp + 2*x0*(1 - e_mr_t)/mr_t)
 
             tmp = self.vov / self.mr
-            strike += self.theta*tmp * (tmp/4 - self.rho) * (1 - (1-e_mr_a)/mr_a)
-            strike += x0 * tmp * (tmp/2 - self.rho) * (1 - e_mr)/mr_t * (1 + mr_a/(1-e_mr_a))
-            strike -= (tmp**2*(self.mr - 2*var0) + 2*x0**2/self.mr) * (1 - e_mr**2)/(8*mr_t) * (1-e_mr_a)/(1+e_mr_a)
+            strike += self.theta * tmp * (tmp/4 - self.rho) * (1 - (1-e_mr_a)/mr_a)
+            strike += x0 * tmp * (tmp/2 - self.rho) * (1 - e_mr_t)/mr_t * (1 + mr_a/(1 - 1/e_mr_a))
+            strike += (tmp**2*(self.theta - 2*var0) + 2*x0**2/self.mr) * (1 - e_mr_t**2)/(8*mr_t) * (1-e_mr_a)/(1+e_mr_a)
 
         return strike
 
@@ -98,7 +105,7 @@ class HestonUncorrBallRoma1994(HestonABC):
         if not np.isclose(self.rho, 0.0):
             print(f"Pricing ignores rho = {self.rho}.")
 
-        avgvar, var = self.avgvar_mv(self.sigma, texp)
+        avgvar, var = self.avgvar_mv(texp)
 
         m_bs = bsm.Bsm(np.sqrt(avgvar), intr=self.intr, divr=self.divr)
         price = m_bs.price(strike, spot, texp, cp)

pyfeng/heston_mc.py
@@ -132,7 +132,7 @@ class HestonMcABC(heston.HestonABC, sv.CondMcBsmABC, abc.ABC):
     def cond_spot_sigma(self, var_0, texp):
         var_final, var_avg = self.cond_states(var_0, texp)
 
-        avgvar_m, avgvar_v = self.avgvar_mv(var_0, texp)
+        avgvar_m, avgvar_v = self.avgvar_mv(texp, var_0)
         self.result = {**self.result,
                        'avgvar mean': avgvar_m,
                        'avgvar mean error': var_avg.mean()/avgvar_m - 1,
@@ -140,7 +140,7 @@ class HestonMcABC(heston.HestonABC, sv.CondMcBsmABC, abc.ABC):
                        'avgvar var error': np.square(var_avg - avgvar_m).mean()/avgvar_v - 1
                        }
 
-        spot_cond = ((var_final - var_0) - self.mr * texp * (self.theta - var_avg)) / self.vov \
+        spot_cond = ((var_final - var_0) + self.mr * texp * (var_avg - self.theta)) / self.vov \
              - 0.5 * self.rho * var_avg * texp
         np.exp(self.rho * spot_cond, out=spot_cond)
         sigma_cond = np.sqrt((1.0 - self.rho**2) / var_0 * var_avg)  # normalize by initial variance
@@ -161,8 +161,8 @@ class HestonMcABC(heston.HestonABC, sv.CondMcBsmABC, abc.ABC):
         Returns:
             log return
         """
-        mean_ln = self.rho * ((var_t - var_0) - self.mr * dt * (self.theta - var_avg)) / self.vov \
-            - (self.intr - 0.5 * var_avg) * dt
+        mean_ln = self.rho/self.vov * ((var_t - var_0) + self.mr * dt * (var_avg - self.theta))  \
+            + (self.intr - 0.5 * var_avg) * dt
         sigma_ln = np.sqrt((1.0 - self.rho**2) * var_avg * dt)
         zn = self.rv_normal(spawn=4)
         return mean_ln + sigma_ln * zn
@@ -890,7 +890,7 @@ class HestonMcPoisTimeStep(HestonMcABC):
         """
 
         dt = dt or self.dt
-        mean, var = self.avgvar_mv(self.sigma, texp)
+        mean, var = self.avgvar_mv(texp)
 
         m_x, v_x = self.x1star_avgvar_mv(dt, kk=0)
         m_z, v_z = self.x2star_avgvar_mv(dt, kk=0)