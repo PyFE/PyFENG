d9aaa1697de2acf485cf374adf1e7a7627cfa8fc
Jaehyuk Choi <jaehyukchoi@users.noreply.github.com>
2024-10-06 01:01:54 -0400

    SabrMixture moved to __init__

--- pyfeng/__init__.py
+++ pyfeng/__init__.py
@@ -12,7 +12,7 @@ from .sv_fft import HestonFft, BsmFft, OusvFft, VarGammaFft, CgmyFft, ExpNigFft,
 
 # SABR/NSVh related models
 from .sabr import SabrHagan2002, SabrNormVolApprox, SabrLorig2017, SabrChoiWu2021H, SabrChoiWu2021P
-from .sabr_int import SabrUncorrChoiWu2021, SabrNormAnalytic, SabrNormEllipeInt
+from .sabr_int import SabrUncorrChoiWu2021, SabrNormAnalytic, SabrNormEllipeInt, SabrMixture
 from .sabr_mc import SabrMcTimeDisc
 from .nsvh import Nsvh1, NsvhMc, NsvhGaussQuad
 
