f4187df674a03790eb2eeef3e0feb56994d59fcd
Jaehyuk Choi <jaehyukchoi@users.noreply.github.com>
2024-10-25 14:58:27 +0800

    Update american.py

--- pyfeng/american.py
+++ pyfeng/american.py
@@ -32,7 +32,7 @@ class AmerLi2010QdPlus(OptABC):
         Returns:
             Exercise boundary (critical stock price)
         """
-        s0 = strike * np.fmin(self.divr, self.intr)/self.divr
+        s0 = strike * np.fmin(1, self.intr/np.fmax(self.divr, np.finfo(float).tiny))
         mm = 2*self.intr / self.sigma**2
         nn_m1 = 2*(self.intr - self.divr) / self.sigma**2 - 1
         q_inf = -0.5 * (nn_m1 + np.sqrt(nn_m1**2 + 4*mm))
@@ -53,16 +53,17 @@ class AmerLi2010QdPlus(OptABC):
         Returns:
             Exercise boundary (critical stock price)
         """
-        root = spopt.root(self.zero_func, x0=strike, args=(strike, texp, cp))
-        return root
+        root = spopt.root(self.zero_func, x0=strike*np.ones_like(texp), args=(strike, texp, cp))
+        rv = root.x[0] if np.isscalar(texp) else root.x
+        return rv
 
-    def zero_func(self, bdd, strike, texp, cp=-1):
+    def zero_func(self, spot_bdd, strike, texp, cp=-1):
         """
         Function to solve for QD+ method.
         Eq. (34) of Li (2010) with c replaced with c0 or Eq. (66) of Andersen et al. (2016)
 
         Args:
-            bdd: boundary
+            spot_bdd: boundary
             strike: strike price
             texp: time to expiry
             cp: 1/-1 for call/put option
@@ -71,7 +72,7 @@ class AmerLi2010QdPlus(OptABC):
             function value
         """
 
-        fwd, df, divf = self._fwd_factor(bdd, texp)
+        fwd, df, divf = self._fwd_factor(spot_bdd, texp)
 
         sigma_std = np.maximum(self.sigma*np.sqrt(texp), np.finfo(float).tiny)
         d1 = np.log(fwd/strike)/sigma_std
@@ -84,14 +85,51 @@ class AmerLi2010QdPlus(OptABC):
         qqd_sqrt = np.sqrt(nn_m1**2 + 4*mm/hh)
         qqd_prime = mm / (hh**2 * qqd_sqrt)
 
-        p_euro = self.bsm_model.price(bdd, strike, texp, cp=cp)
-        theta_euro = self.bsm_model.theta(bdd, strike, texp, cp=cp)
+        p_euro = self.bsm_model.price(strike, spot_bdd, texp, cp=cp)
+        theta = self.bsm_model.theta(strike, spot_bdd, texp, cp=cp)
 
-        qqd_c0 = mm/qqd_sqrt * (df/hh - theta_euro/self.intr/(strike - bdd - p_euro) - df*qqd_prime/qqd_sqrt)
+        qqd_c0 = mm/qqd_sqrt * (df / hh - theta / self.intr / (strike - spot_bdd - p_euro) - df * qqd_prime / qqd_sqrt)
         qqd_c0 -= 0.5*(nn_m1 + qqd_sqrt)
 
-        zero = (1 - divf*spst.norm._cdf(-d1))*bdd + qqd_c0*(strike - bdd - p_euro)
+        zero = (1 - divf*spst.norm._cdf(-d1)) * spot_bdd + qqd_c0 * (strike - spot_bdd - p_euro)
         return zero
 
-    def price(self):
-        pass
+    def price(self, strike, spot, texp, cp=-1):
+        """
+        American call/put option price. Eq. (29) of Li (2010)
+
+        Args:
+            strike: strike price.
+            spot: spot (or forward) price.
+            texp: time to expiry.
+            cp: 1/-1 for call/put option.
+
+        Returns:
+            option price
+        """
+
+        assert cp < 0
+        fwd, df, divf = self._fwd_factor(spot, texp)
+        spot_bdd = self.exer_bdd(strike, texp, cp=cp)
+
+        if spot <= spot_bdd:
+            return strike - spot
+
+        p_euro = self.bsm_model.price(strike, spot_bdd, texp, cp=cp)
+        theta = self.bsm_model.theta(strike, spot_bdd, texp, cp=cp)
+
+        nn_m1 = 2*(self.intr - self.divr) / self.sigma**2 - 1
+        mm = 2*self.intr / self.sigma**2
+        hh = 1.0 - df
+
+        qqd_sqrt = np.sqrt(nn_m1**2 + 4*mm/hh)
+        qqd_prime = mm / (hh**2 * qqd_sqrt)
+        qqd = -0.5*(nn_m1 + qqd_sqrt)
+        c0 = mm/qqd_sqrt * (df/hh - theta/self.intr/(strike - spot_bdd - p_euro) - df*qqd_prime/qqd_sqrt)
+        b = -df * mm * qqd_prime / (2*qqd_sqrt)
+
+        log = np.log(spot/spot_bdd)
+        p_amer = self.bsm_model.price(strike, spot, texp, cp=cp) \
+                 + (strike - spot_bdd - p_euro) / (1 - log*(c0 + b*log)) * np.exp(qqd*log)
+
+        return p_amer
