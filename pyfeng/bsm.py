Various numpy broadcast issues fixed

pyfeng/bsm.py
@@ -307,14 +307,14 @@ class Bsm(opt.OptAnalyticABC):
         p_min = bsm_model.price(strike_std, 1.0, texp, cp)
         bsm_model.sigma = np.inf
         p_max = bsm_model.price(strike_std, 1.0, texp, cp)
-        scalar_output = np.isscalar(p_min) & np.isscalar(price_std)
+        scalar_output = np.isscalar(p_min) and np.isscalar(price_std)
 
         # Exclude optoin price below intrinsic value or above max value (1 for call or k for put)
         # ind_solve can be scalar or array. scalar can be fine in np.abs(p_err[ind_solve])
         ind_solve = (price_std - p_min > self.IMPVOL_TOL) & (p_max - price_std > self.IMPVOL_TOL)
 
         # initial guess = inflection point in sigma (volga=0)
-        _sigma = np.ones_like(ind_solve)*np.sqrt(2*np.abs(np.log(strike_std))/texp)
+        _sigma = np.broadcast_to(np.sqrt(2*np.abs(np.log(strike_std))/texp), np.shape(ind_solve)).copy()
 
         bsm_model.sigma = _sigma
         p_err = bsm_model.price(strike_std, 1.0, texp, cp) - price_std
@@ -442,7 +442,7 @@ class Bsm(opt.OptAnalyticABC):
             volatility smile under the specified model
         """
         if model.lower() == "bsm":
-            return self.sigma * np.ones_like(strike) * np.ones_like(spot) * np.ones_like(texp)
+            return np.full(np.broadcast_shapes(np.shape(strike), np.shape(spot), np.shape(texp)), self.sigma)
         if model.lower() == "norm":
             if cp is None:
                 fwd = self.forward(spot, texp)

pyfeng/norm.py
@@ -262,7 +262,7 @@ class Norm(opt.OptAnalyticABC):
             volatility smile under the specified model
         """
         if model.lower() == "norm":
-            return self.sigma * np.ones_like(strike) * np.ones_like(spot) * np.ones_like(texp) * np.ones_like(cp)
+            return np.full(np.broadcast_shapes(np.shape(strike), np.shape(spot), np.shape(texp)), self.sigma)
         if model.lower() == "bsm":
             if cp is None:
                 fwd = self.forward(spot, texp)

pyfeng/opt_abc.py
@@ -154,7 +154,7 @@ class OptABC(abc.ABC):
         model.sigma = self.IMPVOL_MAXVOL
         p_max = model.price(kk, 1, texp, cp)
 
-        scalar_output = np.isscalar(price) & np.isscalar(p_min)
+        scalar_output = np.isscalar(price) and np.isscalar(p_min)
         ones_like = np.ones_like(np.atleast_1d(price + p_min))
 
         sigma = np.empty(ones_like.shape).flatten()